40a39b578b0e7b6c77a4a6c33bf0352f884ee00d
aravdaite <aravdaite@gmail.com>
2020-09-20 23:00:58 +0200

    f

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', process.env.DOMAIN);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', '*');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
