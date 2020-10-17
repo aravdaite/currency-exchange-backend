c4b8ea149a233f381226e353bd9a77045c8f4ee7
aravdaite <aravdaite@gmail.com>
2020-10-17 18:09:30 +0200

    Revert "m"
    This reverts commit e4ad8f567a27f9b81a9133b5eba7bec89fc3d979.

--- models/Currency.js
+++ models/Currency.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const CurrencySchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true],
-		unique: true,
-	},
-	lt: {
-		type: String,
-		required: [true],
-		unique: false,
-	},
-	en: {
-		type: String,
-		required: [true],
-		unique: false,
-	},
+    name: {
+        type: String,
+        required: [true],
+        unique: true,
+    },
+    lt: {
+        type: String,
+        required: [true],
+        unique: false,
+    },
+    en: {
+        type: String,
+        required: [true],
+        unique: false,
+    },
 });
 
 module.exports = mongoose.model('Currency', CurrencySchema);
